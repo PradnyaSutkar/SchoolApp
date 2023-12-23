ac07defb4532508fd402d682653dd2336cf321d1
Parag Ghadge <154050274+PradnyaSutkar@users.noreply.github.com>
2023-12-23 14:11:36 +0530

    Modified contact details

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -9,24 +9,22 @@ const Footer = () => {
                 <Row>
                     <Col lg="3" md="6" className="m-b-30">
                         <h5 className="m-b-20">Address</h5>
-                        <p>Sanjay Gandhi Nagar, Vijapur Road, Solapur - 4</p>
+                        <p>Sanjay Gandhi Nagar, Vijapur Road, Solapur - 413004</p>
                     </Col>
                     <Col lg="3" md="6" className="m-b-30">
                         <h5 className="m-b-20">Phone</h5>
-                        <p>Reception :  + <br />Office :  +</p>
+                        <p>Reception :  +91-7276187542</p>
                     </Col>
                     <Col lg="3" md="6" className="m-b-30">
                         <h5 className="m-b-20">Email</h5>
-                        <p>Office :  <a href="#" className="link">info@wrappixel.com</a> <br />Site :  <a href="#" className="link">wrapkit@wrappixel.com</a></p>
+                        <p>Office :  <a href="#" className="link">samrat.ashok.yuvak.<br/>mandal1982@gmail.com</a> <br />Site :  <a href="#" className="link"></a></p>
                     </Col>
                     <Col lg="3" md="6">
                         <h5 className="m-b-20">Social</h5>
                         <div className="round-social light">
-                            <a href="#" className="link"><i className="fa fa-facebook"></i></a>
-                            <a href="#" className="link"><i className="fa fa-twitter"></i></a>
-                            <a href="#" className="link"><i className="fa fa-google-plus"></i></a>
-                            <a href="#" className="link"><i className="fa fa-youtube-play"></i></a>
-                            <a href="#" className="link"><i className="fa fa-instagram"></i></a>
+                            <a href="#" className="https://www.facebook.com/samrat.ashok.yuvak.mandal?mibextid=ZbWKwL"><i className="fa fa-facebook"></i></a><br />
+                            <a href="#" className="https://youtube.com/@samrat_shikshan?si=bfLXV8BVru0YGDJg"><i className="fa fa-youtube-play"></i></a><br />
+                            <a href="#" className="https://www.instagram.com/yashodhara_campus?igsh=OGQ5ZDc2ODk2ZA=="><i className="fa fa-instagram"></i></a>
                         </div>
                     </Col>
                 </Row>
--- src/views/donate/sections/bannercomponent.jsx
+++ src/views/donate/sections/bannercomponent.jsx
@@ -11,8 +11,8 @@ const BannerComponent = () => {
                         <Col md="7" className="text-center">
                             <h4 className="title font-bold">Indian and foreign citizens can donate to 'Samrat Ashok Yuvak Mandal' via bank transfer or cheque. Please contact on the below e-mail or Phone number for further details.</h4>
                             <h6 className="subtitle">Samrat Ashok Yuvak Mandal Accounts Office, Sanjay Gandhi Nagar, Vijapur Road, Solapur - 413004.</h6>
-                            <h6 className="subtitle">Contact:</h6>
-                            <h6 className="subtitle">Email:</h6>
+                            <h6 className="subtitle">Contact: +91-7276187542</h6>
+                            <h6 className="subtitle">Email: samrat.ashok.yuvak.mandal1982@gmail.com</h6>
                         </Col>
                     </Row>
                 </Container>
